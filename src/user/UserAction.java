73ecb63fb67148521d3e3c0462dfee509a5c7302
XLongGitHub <1311620554@qq.com>
2017-08-04 11:37:42 +0800

    查看所有用户，遍历包含对象的list

--- src/user/UserAction.java
+++ src/user/UserAction.java
@@ -3,12 +3,11 @@ package user;
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 import database.DB;
-import org.apache.struts2.ServletActionContext;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Map;
+import java.util.*;
 
 public class UserAction extends ActionSupport {
     private String phone;
@@ -18,6 +17,84 @@ public class UserAction extends ActionSupport {
     private String grade;
     private String avactor;
     private String address;
+    private List<User> users = new LinkedList<>();
+
+    class User {
+        private String u_name;
+        private String u_avator;
+        private String u_sex;
+        private String u_address;
+        private String u_phone;
+        private String u_grade;
+        private String u_create_time;
+
+
+        public User(String u_name, String u_avator, String u_sex, String u_address, String u_phone, String u_grade, String u_create_time) {
+            this.u_name = u_name;
+            this.u_avator = u_avator;
+            this.u_sex = u_sex;
+            this.u_address = u_address;
+            this.u_phone = u_phone;
+            this.u_grade = u_grade;
+            this.u_create_time = u_create_time;
+        }
+
+        public String getU_name() {
+            return u_name;
+        }
+
+        public void setU_name(String u_name) {
+            this.u_name = u_name;
+        }
+
+        public String getU_avator() {
+            return u_avator;
+        }
+
+        public void setU_avator(String u_avator) {
+            this.u_avator = u_avator;
+        }
+
+        public String getU_sex() {
+            return u_sex;
+        }
+
+        public void setU_sex(String u_sex) {
+            this.u_sex = u_sex;
+        }
+
+        public String getU_address() {
+            return u_address;
+        }
+
+        public void setU_address(String u_address) {
+            this.u_address = u_address;
+        }
+
+        public String getU_phone() {
+            return u_phone;
+        }
+
+        public void setU_phone(String u_phone) {
+            this.u_phone = u_phone;
+        }
+
+        public String getU_grade() {
+            return u_grade;
+        }
+
+        public void setU_grade(String u_grade) {
+            this.u_grade = u_grade;
+        }
+
+        public String getU_create_time() {
+            return u_create_time;
+        }
+
+        public void setU_create_time(String u_create_time) {
+            this.u_create_time = u_create_time;
+        }
+    }
 
     /**
      * 判断该用户是否存在
@@ -122,6 +199,51 @@ public class UserAction extends ActionSupport {
         }
     }
 
+    /**
+     * 根据条件查看所有用户
+     * @return
+     */
+    public String users() {
+        ResultSet usersSet =  DB.executeQuery("SELECT avactor,name,sex,address,phone, grade, create_time FROM s_user");
+        try {
+            while (usersSet.next()){
+                //map 根据key-value去去掉多余部分
+//                users.put("name", usersSet.getString("name"));
+//                users.put("avactor", usersSet.getString("avactor"));
+//                users.put("sex", "" + usersSet.getInt("sex"));
+//                users.put("address", usersSet.getString("address"));
+//                users.put("phone", usersSet.getString("phone"));
+//                users.put("grade", usersSet.getString("grade"));
+//                users.put("create_time", usersSet.getString("create_time"));
+
+                String u_name = usersSet.getString("name");
+                String u_avator =   usersSet.getString("avactor");
+                String u_sex = "" + usersSet.getInt("sex");
+                String u_address = usersSet.getString("address");
+                String u_phone = usersSet.getString("phone");
+                String u_grade = "" + usersSet.getInt("grade");
+                String u_create_time = usersSet.getString("create_time");
+
+                users.add(new User(u_name, u_avator, u_sex, u_address, u_phone, u_grade, u_create_time));
+            }
+            System.out.println(
+                    users.size()
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "users_success";
+//        if (rs.next())
+    }
+
+    private static String fillEmpty(String str) {
+        if (str == null) {
+            str = "empty";
+        } else if (str != null && str.equals("")) {
+            str = "empty";
+        }
+        return  str;
+    }
     public String getPassword() {
         return password;
     }
@@ -176,5 +298,13 @@ public class UserAction extends ActionSupport {
 
     public void setAddress(String address) {
         this.address = address;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public List<User> getUsers() {
+        return users;
     }
 }
