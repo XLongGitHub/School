e34d1738a3ceae10e43d1b49bcb034e3093449aa
XLongGitHub <1311620554@qq.com>
2017-08-04 15:57:32 +0800

    查看课程信息

--- src/course/CourseAction.java
+++ src/course/CourseAction.java
@@ -2,8 +2,12 @@ package course;
 
 import database.DB;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 public class CourseAction {
     private String name;
@@ -11,6 +15,73 @@ public class CourseAction {
     private String schooltime_id;
     private String create_time;
     private String write_time;
+    private List<Course> courses;
+
+    class Course {
+        private String id;
+        private String name;
+        private String classroom_id;
+        private String schooltime_id;
+        private String create_time;
+        private String write_time;
+
+        public Course(String id, String name, String classroom_id, String schooltime_id, String create_time, String write_time) {
+            this.id = id;
+            this.name = name;
+            this.classroom_id = classroom_id;
+            this.schooltime_id = schooltime_id;
+            this.create_time = create_time;
+            this.write_time = write_time;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getClassroom_id() {
+            return classroom_id;
+        }
+
+        public void setClassroom_id(String classroom_id) {
+            this.classroom_id = classroom_id;
+        }
+
+        public String getSchooltime_id() {
+            return schooltime_id;
+        }
+
+        public void setSchooltime_id(String schooltime_id) {
+            this.schooltime_id = schooltime_id;
+        }
+
+        public String getCreate_time() {
+            return create_time;
+        }
+
+        public void setCreate_time(String create_time) {
+            this.create_time = create_time;
+        }
+
+        public String getWrite_time() {
+            return write_time;
+        }
+
+        public void setWrite_time(String write_time) {
+            this.write_time = write_time;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+    }
 
     /**
      * 添加课程信息
@@ -36,6 +107,11 @@ public class CourseAction {
      * @return
      */
     public String modify() {
+        if (name == null) {
+            return "modify";
+        } else {
+
+        }
         return "success";
     }
 
@@ -44,6 +120,7 @@ public class CourseAction {
      * @return
      */
     public String delete() {
+
         return "success";
     }
 
@@ -52,7 +129,24 @@ public class CourseAction {
      * @return
      */
     public String get() {
-        return "success";
+        courses = new LinkedList<>();
+        String sql = "select * from s_course";
+        ResultSet rs = DB.executeQuery(sql);
+        try {
+            while (rs.next()) {
+                String c_id = "" + rs.getInt("id");
+                String c_name = rs.getString("name");
+                String c_classroom_id = "" + rs.getInt("classroom_id");
+                String c_schooltime_id = "" + rs.getInt("schooltime_id");
+                String c_create_time = rs.getString("create_time");
+                String c_write_time = rs.getString("write_time");
+                courses.add(new Course(c_id, c_name, c_classroom_id, c_schooltime_id, c_create_time, c_write_time));
+            }
+            return "getCourse_success";
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "error";
     }
 
     public String getName() {
@@ -93,5 +187,13 @@ public class CourseAction {
 
     public void setWrite_time(String write_time) {
         this.write_time = write_time;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
     }
 }
---
+++ web/WEB-INF/content/course/showCourse.jsp
@@ -0,0 +1,46 @@
+<%@ taglib prefix="s" uri="/struts-tags" %>
+<%--
+  Created by IntelliJ IDEA.
+  User: Xulong
+  Date: 2017/8/4
+  Time: 15:34
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>所有课程</title>
+    <link href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<table class="table  table-bordered">
+    <caption>所有课程</caption>
+    <tr>
+        <th>课程名</th>
+        <th>教室</th>
+        <th>上课时间</th>
+        <th>创建时间</th>
+        <th>修改时间</th>
+        <th>操作</th>
+    </tr>
+    
+    <s:iterator value="courses">
+        <tr>
+            <td><s:property value="name"/> </td>
+            <td><s:property value="classroom_id"/> </td>
+            <td><s:property value="schooltime_id"/> </td>
+            <td><s:property value="create_time"/> </td>
+            <td><s:property value="write_time"/> </td>
+            <td>
+                <a href="modifyCourseAction?id=<s:property value="id"/>">
+                    修改
+                </a>
+                <a href="deleteCourseAction?id=<s:property value="id"/>">
+                    删除
+                </a>
+            </td>
+        </tr>
+    </s:iterator>
+</table>
+</body>
+</html>
